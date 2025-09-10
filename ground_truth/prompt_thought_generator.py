64fce32dae229c469dff1e8ea059a36c45fa1844
DANISH CONTRACTOR <Danish.Contractor@ibm.com>
2025-09-10 15:12:37 -0400

    Updates to prompt

--- ground_truth/prompt_thought_generator.py
+++ ground_truth/prompt_thought_generator.py
@@ -30,7 +30,7 @@ You are provided with:
 Tool Use Policy: {tool_policy}
 
 Your task is to generate a concise, coherent thought for each step that must:
-    - Explain why the tool call at that step is reasonable and necessary, grounded in it's tool descriptions and the sub-question. However, note that if a tool response indicates a Policy Error, your thought should say that you wanted to use the tool but you cannot since its not in policy as per the Tool Use policy referenced above (you should restate a summary of the policy for clarity). And thus, if the tool is not in policy, the query cannot be answered by you as you needed the tool to answer the question. DO NOT MENTION THE POLICY ERROR. Here's a clue, document retrievers have the word 'retriever' in their name.
+    - Explain why the tool call at that step is reasonable and necessary, grounded in it's tool descriptions and the sub-question. However, note that if a tool response indicates a Policy Error, your thought should say that you wanted to use the tool but you cannot since its not in policy as per the Tool Use policy referenced above (you should restate a summary of the policy for clarity). And thus, if the use of the tool is not policy, the query cannot be answered by you as you needed the tool to answer the question. DO NOT MENTION THE POLICY ERROR. Here's a clue, document retrievers have the word 'retriever' in their name.
     - Reflect how the tool use policy affects the choice of tools. 
     - Reflect how a tool's arguments were determined, based on reasoning so far and any previous tool responses
     - Naturally follows from the previous step’s thought and outcome
