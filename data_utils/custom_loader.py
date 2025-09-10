4d307e2d77f207274a8fbb376593754c5dccf422
Benjamin Elder Benjamin.Elder@ibm.com <belder@ccc-login4.pok.ibm.com>
2025-09-10 11:16:53 -0400

    revert small change

--- data_utils/custom_loader.py
+++ data_utils/custom_loader.py
@@ -455,7 +455,7 @@ class BaseDataset(TorchDataset):
             yield self[i]
 
     def print_data_sample(self):
-        for _ in range(0):
+        for _ in range(2):
             # Get a random sample from the dataset
             random_sample = random.choice(self.processed_samples)
             if random_sample is None:
