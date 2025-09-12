22ec46325c0ca715dd5e78ce15b7a29ed45928d6
Danish Contractor <danish.contractor.007@gmail.com>
2025-09-12 13:00:26 -0400

    Danish/scenarios for experts (#20)

--- envs/tool_call_env.py
+++ envs/tool_call_env.py
@@ -170,7 +170,7 @@ class ToolCallEnv(BaseEnv):
         # Setup user queries
         logger.info("Setting up User Queries")
         self.setup_user_queries()
-
+        
         # Setup tools
         logger.info("Setting up Tools")
         self.setup_tools()
