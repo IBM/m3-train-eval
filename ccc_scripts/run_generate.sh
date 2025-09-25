ce7c35e410d142517c74dd692d418f64738cc24d
ankita-naik <ankita.naik@ibm.com>
2025-09-25 18:00:19 -0400

    Update the script to run no thoughts version

--- ccc_scripts/run_generate.sh
+++ ccc_scripts/run_generate.sh
@@ -23,14 +23,30 @@ export PYTHONPATH=.
 RUN_DATE=$(date "+%m%d")
 mkdir -p runs/${RUN_DATE}
 
-DB_ID=( "address" "airline" "app_store" "authors" "beer_factory" "bike_share_1" "book_publishing_company" "books" "cars" "chicago_crime" "citeseer" "codebase_comments" "coinmarketcap" "college_completion" "computer_student" "cookbook" "disney" "european_football_1" "food_inspection" "genes" "hockey" "ice_hockey_draft" "image_and_language" "law_episode" "mental_health_survey" "menu" "mondial_geo" "movie_3" "movielens" "movie" "movies_4" "music_tracker" "olympics" "professional_basketball" "public_review_platform" "restaurant" "sales_in_weather" "shakespeare" "simpson_episodes" "soccer_2016" "student_loan" "talkingdata" "trains" "university" "video_games" "world_development_indicators" "world" )
+# # TRAIN DOMAINS
+# DB_ID=( "address" "airline" "app_store" "authors" "beer_factory" "bike_share_1" "book_publishing_company" "books" "cars" "chicago_crime" "citeseer" "codebase_comments" "coinmarketcap" "college_completion" "computer_student" "cookbook" "disney" "european_football_1" "food_inspection" "genes" "hockey" "ice_hockey_draft" "image_and_language" "law_episode" "mental_health_survey" "menu" "mondial_geo" "movie_3" "movielens" "movie" "movies_4" "music_tracker" "olympics" "professional_basketball" "public_review_platform" "restaurant" "sales_in_weather" "shakespeare" "simpson_episodes" "soccer_2016" "student_loan" "talkingdata" "trains" "university" "video_games" "world_development_indicators" "world" )
 
-INPUT_DIR="data/chunked_balanced_rest_v2"
-OUTPUT_DIR="ground_truth/balanced_rest_v2/"
+# INPUT_DIR="data/chunked_balanced_rest_v2"
+# OUTPUT_DIR="ground_truth/balanced_rest_v2/"
+
+# TEST DOMAINS with and without scenarios
+DB_ID=( "world" "disney" "cars" "authors" "computer_student" "music_tracker" "menu" "sales_in_weather" "student_loan" "address" "professional_basketball" "citeseer" "coinmarketcap" "books" "mondial_geo" "shakespeare" "book_publishing_company" "soccer_2016" "talkingdata" "beer_factory" "restaurant" "cookbook" "mental_health_survey" "app_store" "bike_share_1" "world_development_indicators" "european_football_1" "law_episode" "genes" "food_inspection" "university" "ice_hockey_draft" "airline" "image_and_language" "college_completion" "hockey" "olympics" "codebase_comments" "trains" )
+INPUT_DIR="/proj/m3benchmark/m3data/0923/data/test/multi/no_scenarios/chunked"
+OUTPUT_DIR="/proj/m3benchmark/m3data/0923/data/test/multi/no_scenarios/generate"
+
+# # OOD MULTI DOMAINS with and without scenarios
+# DB_ID=( "movie_3" "movie" "movielens" "video_games" "chicago_crime" "simpson_episodes" "public_review_platform" "movies_4" )
+# INPUT_DIR="/proj/m3benchmark/m3data/0923/data/ood/multi/no_scenarios/chunked"
+# OUTPUT_DIR="/proj/m3benchmark/m3data/0923/data/ood/multi/no_scenarios/chunked"
+
+# # OOD SINGLE DOMAINS with and without scenarios
+# DB_ID=( "movie_3" "movie" "movielens" "video_games" "chicago_crime" "simpson_episodes" "public_review_platform" "movies_4" )
+# INPUT_DIR="data/chunked_balanced_rest_v2"
+# OUTPUT_DIR="ground_truth/balanced_rest_v2/"
 
 CCC_CMD_NO_GPU='bsub -n 1 -U infusion -R "rusage[mem=20GB, cpu=4]"'
 CCC_CMD_GPU='bsub -n 1 -U infusion -R "rusage[mem=20GB, cpu=4]" -gpu num=1'
-GENRATION_CMD="PYTHONPATH=./ python -m ground_truth.generate"
+GENRATION_CMD="PYTHONPATH=./ python ground_truth/generate.py --no_thoughts true"
 
 for id in "${DB_ID[@]}"; do
     gen_cmd_args="-i ${INPUT_DIR} -o ${OUTPUT_DIR} --domain ${id}"
