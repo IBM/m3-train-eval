467d38bb5651ddbff43ac9c23256709c3a52b0ed
ankita-naik <ankita.naik@ibm.com>
2025-10-06 15:56:21 -0400

    Update evaluation script with granite4

--- ccc_scripts/run_evaluation.sh
+++ ccc_scripts/run_evaluation.sh
@@ -25,9 +25,9 @@ RUN_DATE=$(date "+%m%d")
 # Setup the following locations and paramters for the run
 RUN_TYPE="baseline"
 MODEL_NAME=( "mixtral22b" "mistral7b" "granite38b" "granite4" )
-INPUT_TYPE=( "ood_multi_turn_mixed" "ood_single_turn_mixed" "test_multi_turn_mixed")
+INPUT_TYPE=( "ood_multi_turn_mixed" "ood_single_turn_mixed" "test_multi_turn_mixed" )
 INPUT_DIR="/proj/m3benchmark/m3data/0923/evaluation_data"
-OUTPUT_DIR="/proj/m3benchmark/m3data/0923/m3_test_evaluation//${RUN_TYPE}"
+OUTPUT_DIR="/proj/m3benchmark/m3data/0923/m3_test_evaluation/${RUN_TYPE}"
 CONFIG_DIR="/dccstor/arnaik_data/routing/m3-train-eval/config_files/evaluation"
 
 CCC_CMD_NO_GPU='bsub -n 1 -U infusion -R "rusage[mem=20GB, cpu=4]"'
@@ -38,7 +38,7 @@ for mn in "${MODEL_NAME[@]}"; do
     for it in "${INPUT_TYPE[@]}"; do
         mkdir -p runs/${RUN_DATE}/${mn}/${it}
         mkdir -p ${OUTPUT_DIR}/${mn}/${it}/
-        gen_cmd_args="-i ${INPUT_DIR}/${it}.json -o ${OUTPUT_DIR}/${mn}/${it}/ -ic ${CONFIG_DIR/infer_agent_${mn}.json"
+        gen_cmd_args="-i ${INPUT_DIR}/${it}.json -o ${OUTPUT_DIR}/${mn}/${it}/ -ic ${CONFIG_DIR}/infer_agent_${mn}.json"
         g_cmd="${CCC_CMD_GPU} -o runs/${RUN_DATE}/${mn}/${it}/%J_${id}.log ${GENRATION_CMD} ${gen_cmd_args}"
         logHeader "Running: ${g_cmd}"
         eval "$g_cmd"
