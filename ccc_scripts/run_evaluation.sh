5f6131582f78976a5eb8a8ebc16395e0b3d55b3d
ankita-naik <ankita.naik@ibm.com>
2025-10-07 17:31:01 -0400

    Update run_evaluation.sh script to parallelize across subsections. Added a run_mode to be able to run and delete jobs easily.

--- ccc_scripts/run_evaluation.sh
+++ ccc_scripts/run_evaluation.sh
@@ -23,10 +23,14 @@ export PYTHONPATH=.
 RUN_DATE=$(date "+%m%d")
 
 # Setup the following locations and paramters for the run
-RUN_TYPE="baseline"
-MODEL_NAME=( "mixtral22b" "mistral7b" "granite38b" "granite4" )
+RUN_TYPE="dummy" # Update to the required folder
+RUN_MODE="run"
+# MODEL_NAME=( "mixtral22b" "mistral7b" "granite38b" "granite4" )
+MODEL_NAME=( "mistral7b" "granite38b" )
 INPUT_TYPE=( "ood_multi_turn_mixed" "ood_single_turn_mixed" "test_multi_turn_mixed" )
-INPUT_DIR="/proj/m3benchmark/m3data/0923/evaluation_data"
+NUM_SAMPLES=( "400" "800" "1200" "ALL")
+
+INPUT_DIR="/proj/m3benchmark/m3data/0923/evaluation_data/v2"
 OUTPUT_DIR="/proj/m3benchmark/m3data/0923/m3_test_evaluation/${RUN_TYPE}"
 CONFIG_DIR="/dccstor/arnaik_data/routing/m3-train-eval/config_files/evaluation"
 
@@ -38,9 +42,16 @@ for mn in "${MODEL_NAME[@]}"; do
     for it in "${INPUT_TYPE[@]}"; do
         mkdir -p runs/${RUN_DATE}/${mn}/${it}
         mkdir -p ${OUTPUT_DIR}/${mn}/${it}/
-        gen_cmd_args="-i ${INPUT_DIR}/${it}.json -o ${OUTPUT_DIR}/${mn}/${it}/ -ic ${CONFIG_DIR}/infer_agent_${mn}.json"
-        g_cmd="${CCC_CMD_GPU} -o runs/${RUN_DATE}/${mn}/${it}/%J_${id}.log ${GENRATION_CMD} ${gen_cmd_args}"
-        logHeader "Running: ${g_cmd}"
-        eval "$g_cmd"
+        for ns in "${NUM_SAMPLES[@]}"; do
+            INPUT_FILE_NAME=${INPUT_DIR}/${it}_${ns}.json
+            if [ -e "$INPUT_FILE_NAME" ]; then
+                gen_cmd_args="-i ${INPUT_FILE_NAME} -o ${OUTPUT_DIR}/${mn}/${it}/ -ic ${CONFIG_DIR}/infer_agent_${mn}.json"
+                g_cmd="${CCC_CMD_GPU} -J ${RUN_MODE}_${mn}_${it}_${ns} -o runs/${RUN_DATE}/${mn}/${it}/%J_${ns}.log ${GENRATION_CMD} ${gen_cmd_args}"
+                logHeader "Running: ${g_cmd}"
+                eval "$g_cmd"
+            else
+                echo "$INPUT_FILE_NAME does not exist."
+            fi
+        done
     done
 done
